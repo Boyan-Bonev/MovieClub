7c3213a7865162f7e36499dcadc7e4b662fa1228
Boyan-Bonev <bobibonev1@abv.bg>
2025-12-21 18:03:42 +0200

    Skeleton of project

--- src/main/java/com/bhbonev/MovieClub/MovieClubApplication.java
+++ src/main/java/com/bhbonev/MovieClub/MovieClubApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieClubApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(MovieClubApplication.class, args);
 	}
 
